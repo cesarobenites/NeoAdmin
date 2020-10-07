306fc4dd4fc186e5086d455892e44d360af3c5b8
César R <cesar.rcbx@gmail.com>
2020-10-06 23:41:32 -0500

    First version of which I think will be the base structure of front-end projects using Bootstrap + Sass + Js + Webpack

---
+++ src/neoadmin.js
@@ -0,0 +1,14 @@
+/*!
+ * ================================================================
+ * Project: Neo Admin
+ * Author: César R.
+ * Email: cesar.rcbx@gmail.com
+ * Descriptions: General Control
+ * ================================================================
+ */
+
+// Styles
+import './scss/neoadmin.scss';
+
+// JavaScripts
+import './js/neoadmin.js';
